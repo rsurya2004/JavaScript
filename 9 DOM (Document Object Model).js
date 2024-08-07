48259a514d6e627ec76eaf84705897d921d34fb1
Surya <123731796+rsurya2004@users.noreply.github.com>
2024-08-07 12:34:23 +0530

    Add files via upload

---
+++ 9 DOM (Document Object Model).js
@@ -0,0 +1,14 @@
+// DOM (Document Object Model) 
+
+
+
+// Used to JS with HTML, for functionalities
+
+
+// Visualize Of DOM  (Tree-like Structure ,for every site)
+
+// Target using :-  1.Tag  2.Class  3.Id(Unique)
+// Whatever written is a Document
+
+
+
